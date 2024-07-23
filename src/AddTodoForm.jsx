4b71b5ab478c9a771dbf3a0ffbb701fa82cc6a83
Filicity SH <f.elena.sh@gmail.com>
2024-07-22 22:31:18 -0700

    Control "Add Todo" Input

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,24 +1,29 @@
-
+import { useState } from 'react';
 export default function AddTodoForm(props){
 
-const handleAddTodo= (event)=>{
+const [todoTitle , settodoTitle]= useState("");
+const  handleTitleChange =(event)=>{
+    const newTodoTitle=event.target.value;
+    settodoTitle (newTodoTitle);
+};
+
+const handleAddTodo = (event)=>{
     event.preventDefault();
     const form= event.target ;
-    const input = form.querySelector("input");
-    const todoTitle = input.value;
-    console.log(todoTitle);
     props.onAddTodo(todoTitle);
     form.reset();
 };
+
+
 
 return (
 <form onSubmit={handleAddTodo}>
 
 <label htmlFor="todotitle"></label>
-<input id="todotitle" name="title"  />
+<input id="todotitle" name="title" value={todoTitle} onChange= {handleTitleChange} />
 <button type="submit">Add</button>
 
 </form>
 
 );
-}
+};
