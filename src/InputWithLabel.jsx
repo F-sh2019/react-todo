590d77ffa94b8e3ac0531dfb52524245c80961e2
Filicity SH <f.elena.sh@gmail.com>
2024-07-31 09:15:11 -0700

    Add Auto-Focus to Input

--- src/InputWithLabel.jsx
+++ src/InputWithLabel.jsx
@@ -1,12 +1,23 @@
-export  function InputWithLabel(props){
+import {useRef , useEffect} from "react"
+
+export default function InputWithLabel(props){
+
+    const inputRef =useRef() ;
+    
+    useEffect(()=>{
+        inputRef.current.focuse
+    }) ; 
+
+   
     return (
         <>
-            <label htmlFor="todotitle">{props.children}</label>
+            <label htmlFor="todotitle">{props.Children}</label>
             <input 
                 id="todotitle" 
                 name="title"
                 value={props.todoTitle} 
                 onChange= {props.handleTitleChange} 
+                ref={inputRef}
             />
         </>
         )
